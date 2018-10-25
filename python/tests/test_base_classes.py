e61b93eb2b4011e9a418792c029f3ca958021525
Alexander.Olivero <Alexander.Olivero@milliman.com>
2018-10-25 09:12:41 -0400

    Update for pylint and pass in spark_app object to properly test
    Do this to improve pylint score and be able to run test

--- python/tests/test_base_classes.py
+++ python/tests/test_base_classes.py
@@ -7,46 +7,53 @@
 ### DEVELOPER NOTES:
   <none>
 """
+import datetime
 import pytest
 
 from pyspark.sql import Row
 
 from ebm_hedis_etc.base_classes import QualityMeasure
 
+# pylint: disable=unused-variable
+
 # =============================================================================
 # LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
 # =============================================================================
 
 
-def test_qualitymeasure():
-    """"""
-    class test_good_QM(QualityMeasure):
+def test_qualitymeasure(spark_app):
+    """Tests the methods of a QualityMeasure object"""
+    class DummyGoodQM(QualityMeasure):
+        """Dummy quality measure to pass test"""
         def _calc_measure(
                 self,
-                dfs_input: None,
-                performance_yearstart: None
+                dfs_input: "typing.Mapping[str, DataFrame]",
+                performance_yearstart=datetime.date,
+                **kwargs
         ):
             return spark_app.session.createDataFrame(
                 [Row(**{'member_id': 'membera', 'comp_quality_short': 'quality measure 1',
                         'comp_quality_numerator': 1})]
             )
 
-    class test_bad_QM(QualityMeasure):
+    class DummyBadQM(QualityMeasure):
+        """Dummy quality measure to fail test"""
         def _calc_measure(
                 self,
-                dfs_input: None,
-                performance_yearstart: None
+                dfs_input: "typing.Mapping[str, DataFrame]",
+                performance_yearstart=datetime.date,
+                **kwargs
         ):
             return spark_app.session.createDataFrame(
                 [Row(**{'member_id': 'membera', 'short': 'quality measure 1',
                         'comp_quality_numerator': 1})]
             )
 
-    test_qm = test_good_QM()
+    test_qm = DummyGoodQM()
     assert test_qm.key_fields == ['member_id']
     test_results = test_qm.calc_measure(None, None)
 
-    bad_qm = test_bad_QM()
+    bad_qm = DummyBadQM()
     assert bad_qm.key_fields == ['member_id']
     with pytest.raises(AssertionError):
         bad_results = bad_qm.calc_measure(None, None)
