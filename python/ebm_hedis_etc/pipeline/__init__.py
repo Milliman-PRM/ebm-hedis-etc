67d6eebd3735ed42387bccf753627fc5e95b42fd
Alexander Olivero <Alexander.Olivero@milliman.com>
2018-11-27 07:53:51 -0500

    Add a header to the init file in the pipeline dir
    Do this because for documentation and standard practice

---
+++ python/ebm_hedis_etc/pipeline/__init__.py
@@ -0,0 +1,7 @@
+"""
+## CODE OWNERS: Alexander Olivero
+### OBJECTIVE:
+  Mark the pipeline portion of the package to house luigi tasks
+### DEVELOPER NOTES:
+  Not intended to have side-effects
+"""
