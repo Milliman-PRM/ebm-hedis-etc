08ea37f73930d8ec9befbe71d39c741e90c24eb8
Ben Copeland <Ben.Copeland@milliman.com>
2018-11-30 11:30:35 -0500

    Add code owners
    To ensure that QRM collaboration standards are met

--- scripts/statin_therapy_with_cardiovascular.py
+++ scripts/statin_therapy_with_cardiovascular.py
@@ -1,5 +1,5 @@
 """
-### CODE OWNERS: Alexander Olivero
+### CODE OWNERS: Alexander Olivero, Ben Copeland
 
 ### OBJECTIVE:
     Calculate the Statin Therapy for Patients with Cardiovascular Disease (SPC-2) HEDIS measure.
