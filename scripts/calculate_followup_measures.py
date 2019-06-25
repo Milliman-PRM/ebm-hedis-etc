fa2be03b11d79bdb2a99ce2d39ff005ac88652da
Austin Campbell <Austin.Campbell@milliman.com>
2019-06-25 16:46:56 -0400

    Moved logic into calculate_followup_measures(). Fixed convention.

--- scripts/calculate_followup_measures.py
+++ scripts/calculate_followup_measures.py
@@ -20,29 +20,45 @@ PRM_META = parse_project_metadata()
 LOGGER = logging.getLogger(__name__)
 
 PATH_REF = Path(os.environ['EBM_HEDIS_ETC_PATHREF'])
+# pylint: disable=no-member
 
 # =============================================================================
 # LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
 # =============================================================================
 
-
-def main() -> int:
-    """A function to enclose the execution of business logic."""
-    sparkapp = SparkApp(PRM_META['pipeline_signature'])
-
+def calculate_followup_measures(
+        sparkapp: SparkApp
+    ):
+    """calculate 7-day and 14-day PCP follow-up measures"""
     pcp_followup_df = sparkapp.load_df(
-        PRM_META[PRM_META[70, 'out'] / 'med120_prm_pcp_followup.parquet']
+        PRM_META[70, 'out'] / 'med120_prm_pcp_followup.parquet'
     )
 
-    numerator_7_day = pcp_followup_df.between(0, 7).count()
-    numerator_14_day = pcp_followup_df.between(0, 14).count()
+    numerator_7_day = pcp_followup_df.where(
+        spark_funcs.col(
+            'prm_pcp_followup_days_since'
+        ).between(0, 7)
+    ).count()
+
+    numerator_14_day = pcp_followup_df.where(
+        spark_funcs.col(
+            'prm_pcp_followup_days_since'
+        ).between(0, 14)
+    ).count()
 
     if numerator_7_day > numerator_14_day:
-        raise AssertionError('7-day followup success should not be greater than 14-day followup success')
+        raise AssertionError(
+            '7-day followup success should not be greater than 14-day followup success'
+        )
 
     denom = pcp_followup_df.where(
         spark_funcs.col('prm_pcp_followup_potential_yn') == 'Y'
         ).count()
+
+    if denom < numerator_14_day:
+        raise AssertionError(
+            '14-day followup success should not be greater than total potential followup'
+        )
 
     ratio_7_day = numerator_7_day / denom
     ratio_14_day = numerator_14_day / denom
@@ -50,6 +66,13 @@ def main() -> int:
     LOGGER.info('The aggregate prm_pcp_followup_potential_yn is %s', denom)
     LOGGER.info('The 7-day PCP Followup ratio is %s', (ratio_7_day * 100))
     LOGGER.info('The 14-day PCP Followup ratio is %s', (ratio_14_day * 100))
+
+
+def main() -> int:
+    """A function to enclose the execution of business logic."""
+    sparkapp = SparkApp(PRM_META['pipeline_signature'])
+
+    calculate_followup_measures(sparkapp)
 
     return 0
 
