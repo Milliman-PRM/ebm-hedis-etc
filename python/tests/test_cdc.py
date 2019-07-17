29a43993b53fca71a340dd3f91a02b4d7bcd71df
James Holland <James.Holland@milliman.com>
2019-07-17 14:00:40 -0400

    added stub test

---
+++ python/tests/test_cdc.py
@@ -0,0 +1,45 @@
+"""
+### CODE OWNERS: James Holland
+
+### OBJECTIVE:
+    Test the methods that calculate Comprehensive Diabetes Care HEDIS measures
+
+### DEVELOPER NOTES:
+    Currently a stub to be expanded later
+"""
+
+import pytest
+import ebm_hedis_etc.cdc
+
+def test_exclude_elig_gaps():
+    """Testing finding eligibility gaps and excluding members"""
+    assert True
+
+def test_rx_dispensed_event():
+    """Test identification of members who were given insulin or hypoglycemics/antihyperglycemics"""
+    assert True
+
+def test_identify_med_event():
+    """Test identification of members who had diabetes"""
+    assert True
+
+def test_identify_a1c_tests():
+    """Test identification of members with HbA1c during performance year"""
+    assert True
+
+def test_identify_nephropathy():
+    """Test identification of members with screening or evidence for nephropathy"""
+    assert True
+
+def test_identify_eye_exam():
+    """Test identification of screening for diabetic renal disease"""
+    assert True
+
+def test_create_output_table():
+    """Test creation of output table"""
+    assert True
+
+def test_calc_measure():
+    """Test calculation of diabetes care measures"""
+    assert True
+    
