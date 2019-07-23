68b809692e6b251d6a6be0dd497483d4650ff736
James Holland <James.Holland@milliman.com>
2019-07-23 15:36:48 -0400

    updated conftest

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -10,6 +10,9 @@
 """
 # pylint: disable=redefined-outer-name
 import pytest
+import os
+from pathlib import Path
+from shutil import rmtree
 
 import prm.spark.cluster
 from prm.spark.app import SparkApp
@@ -55,3 +58,36 @@ def spark_app(request, spark_cluster):
     request.addfinalizer(_cleanup_app)
 
     return testing_app
+
+def pytest_sessionstart():
+    '''Mock prm_params_input.txt'''
+    prm_local = Path(os.environ['USERPROFILE']) / 'prm_local'
+    temp_path = Path(os.environ['TEMP'])
+
+    params_input = prm_local / 'prm_params_Input.txt'
+    temp_dir = prm_local / 'mock_temp'
+
+    temp_dir.mkdir()
+
+    output_rows = ['Parameter_Name|Parameter_Value_String|Parameter_Value_Int|' \
+    'Parameter_Value_Date|Note']
+    output_rows.append('project_id||||')
+    output_rows.append('M010_Cde||||')
+    output_rows.append('date_crediblestart||||')
+    output_rows.append('path_project_logs|' + str(temp_dir) + '|||')
+    output_rows.append('path_local_root|' + str(temp_path) + '|||')
+    output_rows.append('default_spark_sql_partitions|2|||')
+    output_rows.append('pipeline_signature|ref_unit_test|||')
+    output_rows.append('codegen_prca_model_format|model_name_short $32.' \
+        '        model_name_long $128.        modpathel_name_category $16.        ' \
+        'model_name_order best12.        model_name_format $16.|||')
+
+    with params_input.open('w') as out:
+        for row in output_rows:
+            out.write(row + '\n')
+
+def pytest_sessionfinish():
+    '''Remove temporary directory after tests complete'''
+    temp_dir = Path(os.environ['USERPROFILE']) / 'prm_local'/ 'mock_temp'
+    rmtree(str(temp_dir), ignore_errors=True)
+    
