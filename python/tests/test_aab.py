c7ef570f0ea98fc9ae55bafe2543c3ae91c65c43
Ben Copeland <Ben.Copeland@milliman.com>
2019-07-26 15:33:41 -0400

    Add a stub for AAB measure
    To at least ensure that the module imports cleanly

---
+++ python/tests/test_aab.py
@@ -0,0 +1,16 @@
+"""
+### CODE OWNERS: Ben Copeland
+
+### OBJECTIVE:
+    Test the methods that calculate Avoidance of Antiobiotics for Bronchitis HEDIS measures
+
+### DEVELOPER NOTES:
+    Currently a stub to be expanded later
+"""
+
+# pylint: disable=unused-import
+
+import ebm_hedis_etc.aab
+
+if __name__ == '__main__':
+    pass
