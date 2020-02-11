3488a1ab656ee96af358b200dfedac135e9a797a
Chas Busenburg <chas.busenburg@milliman.com>
2020-02-11 15:04:13 -0500

    Intialize Annual Wellcare Visits files
    This is to give us a better idea of changes from commit to commit

---
+++ python/ebm_hedis_etc/annual_wellcare_visits.py
@@ -0,0 +1,9 @@
+"""
+### CODE OWNERS:
+
+### OBJECTIVE:
+    Calculate the percentage of patients who have had their Annual Wellness visit in the current plan year.
+
+### DEVELOPER NOTES:
+  <none>
+"""
