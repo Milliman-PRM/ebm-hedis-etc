2f97bfb54944e2a99b32ffdfcbe8f208633d7779
Chas Busenburg <chas.busenburg@milliman.com>
2020-02-11 15:55:15 -0500

    Initialize stub for `AWV` object
    This is essentially just making sure that we have an initial outline for
    AWV

--- python/ebm_hedis_etc/annual_wellcare_visits.py
+++ python/ebm_hedis_etc/annual_wellcare_visits.py
@@ -7,3 +7,50 @@
 ### DEVELOPER NOTES:
   <none>
 """
+import datetime
+import typing
+
+import pyspark.sql.functions as spark_funcs
+from ebm_hedis_etc.base_classes import QualityMeasure
+from pyspark.sql import DataFrame
+from pyspark.sql import Row
+
+
+class AWV(QualityMeasure):  # pragma: no cover
+    """ Object to house the logic to calculate AAB measure """
+
+    def _filter_claims_by_date(
+        med_claims: DataFrame, performance_yearstart: datetime.date
+    ) -> DataFrame:
+        """ filter claims to only include in elig year"""
+        ...
+
+    def _identify_eligible_events(
+        med_claims: DataFrame, df_reference: DataFrame, df_reference_awv: DataFrame
+    ) -> DataFrame:
+        """ Should identify all eligible claims that are part of the desired value-set defn"""
+        ...
+
+    def _calc_numerator_flag() -> DataFrame:
+        """ Should output a df with member_id and numerator_flag """
+        ...
+
+    def _calc_denominator_flag():
+        """ Should output a df with a member_id and numerator_flag"""
+        ...
+
+    def _calc_eligible_membership():
+        """ Should output eligible membership to filter upon """
+        ...
+
+    def _identify_excluded_members():
+        """ Exclude appropriate members"""
+        ...
+
+    def _calc_measure(
+        self,
+        dfs_input: typing.Mapping[str, DataFrame],
+        performance_yearstart: datetime.date,
+        **kwargs
+    ) -> DataFrame:
+        ...
--- scripts/annual_wellcare_visits.py
+++ scripts/annual_wellcare_visits.py
@@ -7,3 +7,52 @@
 ### DEVELOPER NOTES:
   <none>
 """
+import logging
+import os
+from pathlib import Path
+
+from ebm_hedis_etc.annual_wellcare_visits import AWV
+from prm.meta.project import parse_project_metadata
+from prm.spark.app import SparkApp
+
+PRM_META = parse_project_metadata()
+LOGGER = logging.getLogger(__name__)
+
+PATH_REF = Path(os.environ["EBM_HEDIS_ETC_PATHREF"])
+
+
+def main() -> int:
+    """A function to enclose the execution of business logic"""
+    sparkapp = SparkApp(PRM_META["pipeline_signature"])
+
+    dfs_input = {
+        "member_time": sparkapp.load_df(PRM_META[35, "out"] / "member_time.parquet"),
+        "member": sparkapp.load_df(PRM_META[35, "out"] / "member.parquet"),
+        "med_claims": sparkapp.load_df(PRM_META[35, "out"] / "outclaims.parquet"),
+        "reference": sparkapp.load_df(PATH_REF / "hedis_codes.parquet"),
+    }
+
+    measure = AWV()
+
+    results_df = measure.calc_measure(dfs_input, PRM_META["date_performanceyearstart"])
+
+    sparkapp.save_df(
+        results_df, PRM_META[150, "out"] / "results_annual_wellcare_visits.parquet"
+    )
+
+    return 0
+
+
+if __name__ == "__main__":
+    # pylint: disable=wrong-import-position, wrong-import-order, ungrouped-imports
+    import sys
+    import prm.utils.logging_ext
+    import prm.spark.defaults_prm
+
+    prm.utils.logging_ext.setup_logging_stdout_handler()
+    SPARK_DEFAULTS_PRM = prm.spark.defaults_prm.get_spark_defaults(PRM_META)
+
+    with SparkApp(PRM_META["pipeline_signature"], **SPARK_DEFAULTS_PRM):
+        RETURN_CODE = main()
+
+    sys.exit(RETURN_CODE)
